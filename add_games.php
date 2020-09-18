6102696648eb4cae418e92d807484c4f9e2f174f
Klavdius <39763793+Klavdius@users.noreply.github.com>
2020-09-18 15:39:47 +0300

    add_game создание записей. отработка записей в бд

--- add_games.php
+++ add_games.php
@@ -1,13 +1,35 @@
 <?php
+	
+	
 	$jsData = file_get_contents('conf_dev_db.json');
 	$jsArrData = json_decode($jsData, true);
 	$db = new mysqli($jsArrData["host"] , $jsArrData["name"] , $jsArrData["password"] , $jsArrData["db_name"]);
 	
+	if ($db->connect_errno) {
+		exit("Connection failed: ");
+    }
 	
-	if (!$db) {
-    die("Connection failed: " . mysqli_connect_error());
+	$content = file_get_contents('FormAddGame.txt');
+	echo $content;
+	
+	if($_POST != null){
+		$NewTitle = $_POST['title_game'];
+		$NewDevel = $_POST['develGame']; //   должен быть выбор из разработчиков
+		$NewPubli = $_POST['publiGame']; //   так же поле издателей. или сделать всё 
+		
+		
+		$otvet = $db->query("SELECT games.title FROM 'games' WHERE games.title = $title");
+		echo $otvet;
+		//$result = $db->query("INSERT INTO games (title, developer_id, publisher_id) VALUES ('$NewTitle','$NewDevel','$NewPubli')");
+		
+		if($result == true){
+			echo "Записалось";
+		}else{
+			echo "Ошибка";
+		}
 	}
-	echo "Connected successfully";
+	
+	
 
 
 
--- games.php
+++ games.php
@@ -1,4 +1,33 @@
 <?php 
 
+class Games{
+	
+	public:
+		function __construct($name_game){
+			$this->title = $name_game;
+			
+		}
+		
+		function SetTitle($new_title);{
+			$this->title = $new_title;
+		}
+		
+		function GetTitle(){
+			return $this->title;
+		}	
+		
+		public write_game_db(){
+			
+			
+		}
+	
+	private:
+	$title;
+	$ganres;
+	$relise_date;
+	$developer;
+	$publisher;
+	$created_time;
+};
 
 ?>
