81d9e3d1f7ecc85c30bc82ecad6005049c8388bb
Klavdius <39763793+Klavdius@users.noreply.github.com>
2020-10-13 15:29:54 +0300

    user

---
+++ addUser.php
@@ -0,0 +1,48 @@
+<?php
+
+	/*
+	Логин пароль
+	почта для проверки
+	*/
+	include 'connectBD.php';
+	
+	if(!empty($_POST)){
+		if($_POST['new_pass'] != $_POST['new_pass2']){
+			echo "Пароли должны совпадать!";
+		}
+		else{
+			$nNewUser = $_POST['new_user'];
+			$nNewMail = $_POST['new_mail'];
+			$nNewPass = $_POST['new_pass'];
+			$nNewDes = $_POST['new_descri'];
+			
+			$NewUser = $db->real_escape_string($nNewUser);
+			$NewMail = $db->real_escape_string($nNewMail);
+			$NewPass = $db->real_escape_string($nNewPass);
+			$NewDes = $db->real_escape_string($nNewDes);
+			$NewDob = $_POST['new_dob'];
+		
+			$result = $db->query("INSERT INTO `users` (`username`,`email`,`password`,`description`,`dob`,`created_at`)VALUES ('$NewUser','$NewMail','$NewPass','$NewDes','$NewDob',NOW())");
+				echo "Профиль создан!";
+		}
+	}
+?>
+
+
+<form method="POST" action="addUser.php">
+	<label>Введите имя (login)</label><br/><br/>
+	<input type=text name='new_user'><br/><br/>
+	<label>Почтовый ящик</label><br/><br/>
+	<input type=text name='new_mail'><br/><br/>
+	<label>Ваш пароль</label><br/>
+	<input type=password name='new_pass'><br/><br/>
+	<label>Повторите пароль</label><br/>
+	<input type=password name='new_pass2'><br/><br/>
+	<label>Пару слов о себе? (необязательно)</label><br/>
+	<input type=text name='new_descri'><br/>
+	<label>День рожденья </label><br/>
+	<input type=date name="new_dob"> <br/><br/>
+	<input type=submit value='Зарегестрироваться'><br/>
+	<a href="http://127.0.0.1/welcom.php">Назад</a>
+		
+</form>
---
+++ welcom.php
@@ -0,0 +1,13 @@
+<?php
+	
+
+?>
+
+<form method='POST' action='welcom.php'>
+	<label> Введите имя </label><br/><br/>
+	<input type=text name='user_name'><br/><br/>
+	<label> Введите пароль</label><br/><br/>
+	<input type=password name='pass_user'><br/><br/>
+	<input type=submit value='Зайти'>
+	<a href='http://127.0.0.1/addUser.php'>Зарегистрироваться </a>
+</form>
